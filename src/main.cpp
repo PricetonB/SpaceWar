b42b570f5defc06e7ec31af91029827d949e8b21
Priceton Braswell <pricetonbraswell@gmail.com>
2025-04-11 04:49:41 -0400

    added a green circle

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color(100,250,50));
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -15,7 +18,10 @@ int main()
             }
         }
 
+	
+
         window.clear();
+	window.draw(shape);
         window.display();
     }
 }
