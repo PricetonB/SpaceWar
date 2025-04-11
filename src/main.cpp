87d94745b004d6d9cb94c4f6acf313dc48ad2874
Priceton Braswell <pricetonbraswell@gmail.com>
2025-04-11 05:22:35 -0400

    rainbow triangle with vertex array

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,22 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
-    sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color(100,250,50));
-
+	
+    // create an array of 3 vertices that define a triangle primitive
+    sf::VertexArray triangle(sf::PrimitiveType::Triangles, 3);
+    
+    // define the position of the triangle's points
+    triangle[0].position = sf::Vector2f(100.f, 100.f);
+    triangle[1].position = sf::Vector2f(1000.f, 100.f);
+    triangle[2].position = sf::Vector2f(1000.f, 800.f);
+    
+    // define the color of the triangle's points
+    triangle[0].color = sf::Color::Red;
+    triangle[1].color = sf::Color::Blue;
+    triangle[2].color = sf::Color::Green;
+    
+    // no texture coordinates here, we'll see that later
+    
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -21,7 +34,7 @@ int main()
 	
 
         window.clear();
-	window.draw(shape);
+	window.draw(triangle);
         window.display();
     }
 }
